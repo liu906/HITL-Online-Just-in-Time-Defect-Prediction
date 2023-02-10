d292229628ae908a8233a84cf32d4ad2430b2792
Xutong Liu <liu906326315@gmail.com>
2023-02-10 18:30:30 +0800

    modify batch script

--- r_script/imbalanceStrategyComparison.r
+++ r_script/imbalanceStrategyComparison.r
@@ -7,30 +7,43 @@ settings <- c(
   'OnlineAdaC2',
   'OnlineRUSBoost',
   'OnlineSMOTEBagging',
-  'OnlineUnderOverBagging',
-  'RebalanceStream'
+  'OnlineUnderOverBagging'
+  #'RebalanceStream' same with no rebalance
 )
 
 subfolder <- 'RQ1-seed2'
 files <-
   list.files("experimentResult/RebalanceStream/RQ1-seed2/", pattern = 'dumpFile')
 
+
 indicators <- c(
   '[avg] Recall for class 1 (percent)',
   '[avg] Kappa Recall Temporal Statistic 1 (percent)',
   '[avg] Gmean for recall  (percent)',
-  '[avg] Kappa Gmean Temporal Statistic  (percent)'
+  '[avg] Kappa Gmean Temporal Statistic  (percent)',
+  '[avg] F1 Score (percent)',
+  '[avg] Precision (percent)',
+  '[avg] classifications correct (percent)'
 )
+
+# indicators <-
+#   c(
+#     '[avg] F1 Score (percent)',
+#     '[avg] Precision (percent)',
+#     '[avg] classifications correct (percent)'
+#   )
+
 
 for (file in files) {
   first_setting <- T
   for (setting in settings) {
+    cat(setting,'*******\n')
     first_flag <- T
     for (indicator in indicators) {
       cat(indicator, '\n')
       
       file_name <-
-      file.path('experimentResult', setting, subfolder, file)
+        file.path('experimentResult', setting, subfolder, file)
       df <- read.csv(file = file_name, check.names = F)
       colnames(df)
       ind_col <- df[, indicator]
@@ -50,25 +63,19 @@ for (file in files) {
     if (first_setting) {
       total_res <- res
       first_setting <- F
-    } else{
+    }else{
       total_res <- rbind(total_res, res)
     }
   }
   
   write.csv(
     total_res,
-    file.path('r_script/result/imbalanceStrategyComparison/', file),
+    file.path('r_script/result/imbalanceStrategyComparison/', paste('',file,sep='')),
     row.names = F,
     quote = F
   )
   
 }
 
-files <- list.files('r_script/result/imbalanceStrategyComparison/',full.names = T)
-
-for(file in files){
-  df <- read.csv(file,check.names = F)
-  write.csv(df,file,row.names = F,quote = F)
-}
-
-
+sum(total_res$setting=='HoeffdingAdaptiveTree')
+sum(total_res$setting=='OnlineUnderOverBagging')
