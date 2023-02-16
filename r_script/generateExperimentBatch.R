78fcbe0461e3e71d7ee8800799c9a625dab93840
Xutong Liu <liu906326315@gmail.com>
2023-02-16 16:49:52 +0800

    add function to automatically generate batch command

--- r_script/generateExperimentBatch.R
+++ r_script/generateExperimentBatch.R
@@ -1,4 +1,27 @@
 
+combineCommand <- function(learner,seed,f_sampleFrequency,q_timeFrequency,project,P,N){
+  command <-
+    paste(
+      'java -classpath "classes" moa.DoTask EvaluatePrequentialDelayedCVPosNegWindow -l ',
+      learner ,
+      ' -s "(ArffFileStream" -f "',
+      project ,
+      ')" -e "(FadingFactorClassificationPerformanceEvaluator" -a 0.99 -o -p -r "-f)" -k 99 -f ',
+      f_sampleFrequency ,
+      ' -q ',
+      q_timeFrequency ,
+      ' -d "',
+      dump_path ,
+      '" -o ',
+      '"',
+      detail_path ,
+      '"',
+      ' -a Bootstrap-Validation  -D 0 -w 5 -A 1 -r 2 -P ', P,' -N ',N,
+      sep = ''
+    )
+  return(command)
+}
+
 
 seed = 2
 setwd('/media/lxt/TOSHIBA EXT/moa/')
@@ -26,43 +49,84 @@ learners = c(
   '(meta.imbalanced.OnlineAdaBoost -l (meta.AdaptiveRandomForest -x (ADWINChangeDetector -a 0.001) -p (ADWINChangeDetector -a 0.01)))',
   '(meta.imbalanced.OnlineRUSBoost -l (meta.AdaptiveRandomForest -x (ADWINChangeDetector -a 0.001) -p (ADWINChangeDetector -a 0.01)))',
   '(meta.imbalanced.OnlineSMOTEBagging -l (meta.AdaptiveRandomForest -x (ADWINChangeDetector -a 0.001) -p (ADWINChangeDetector -a 0.01)))',
-  '(meta.imbalanced.OnlineUnderOverBagging -l (meta.AdaptiveRandomForest -x (ADWINChangeDetector -a 0.001) -p (ADWINChangeDetector -a 0.01)))')
+  '(meta.imbalanced.OnlineUnderOverBagging -l (meta.AdaptiveRandomForest -x (ADWINChangeDetector -a 0.001) -p (ADWINChangeDetector -a 0.01)))'
+)
 
-f_sampleFrequency = '300'
-q_timeFrequency = '300'
+f_sampleFrequency = '1'
+q_timeFrequency = '1'
 
 
-files <- list.files('./commit_guru_dataset',pattern = 'arff',full.names = T)
-projects <- c('brackets',
-              'camel',
-              'edx-platform',
-              'elasticsearch',
-              'FFmpeg',
-              'git',
-              'kubernetes',
-              'mindspore',
-              'tensorflow',
-              'vlc')
-for(i in 1:length(projects)){
-  
+files <-
+  list.files('./commit_guru_dataset',
+             pattern = 'arff',
+             full.names = T)
+# projects <- c('brackets',
+#               'camel',
+#               'edx-platform',
+#               'elasticsearch',
+#               'FFmpeg',
+#               'git',
+#               'kubernetes',
+#               'mindspore',
+#               'tensorflow',
+#               'vlc')
+
+
+
+for (i in 1:length(projects)) {
   project <- files[i]
-  name <- projects[i]
-  for(learner in learners){
+  name <- substr(basename(project), 1, nchar(basename(project)) - 5)
+  
+  for (learner in learners) {
     learner <- '"(meta.OzaBag" -l "trees.HoeffdingTree)"'
     temp <- gsub('"', '', learner)
-    temp <- gsub(' ','_',temp)
-    dump_path <- paste('./r_script/result/differentLearner/',name,'_',temp,'_DelayedCVIdeal_5Fold_FF0.99_dumpFile.csv',sep='')
-    detail_path <- paste('./r_script/result/differentLearner/',name,'_',temp,'_DelayedCVIdeal_5Fold_FF0.99_detail.csv',sep='')
-    # command <- paste('java -classpath "classes" moa.DoTask EvaluatePrequentialDelayedCVIdeal -l ',
-    #                  learner ,' -s "(ArffFileStream" -f "', project ,')" -e 
-    #                "(FadingFactorClassificationPerformanceEvaluator" 
-    #                -a 0.99 -o -p -r "-f)" -k 99 -f ', f_sampleFrequency ,' -q ', q_timeFrequency ,' -d "',
-    #                  dump_path ,'" -o ','"', detail_path ,'"', ' -a Bootstrap-Validation  -D 0 -w 5 -A 1 -r 2 ',sep='')
-    command <- paste('java -classpath "classes" moa.DoTask EvaluatePrequentialDelayedCVIdeal -l ', learner ,' -s "(ArffFileStream" -f "', project ,')" -e "(FadingFactorClassificationPerformanceEvaluator" -a 0.99 -o -p -r "-f)" -k 99 -f ', f_sampleFrequency ,' -q ', q_timeFrequency ,' -d "', dump_path ,'" -o ','"', detail_path ,'"', ' -a Bootstrap-Validation  -D 0 -w 5 -A 1 -r 2 ',sep='')
-    system(command,wait = F)
+    temp <- gsub(' ', '_', temp)
+    dump_path <-
+      paste(
+        './r_script/result/differentLearner/',
+        name,
+        '_',
+        temp,
+        '_DelayedCVIdeal_5Fold_FF0.99_dumpFile.csv',
+        sep = ''
+      )
+    detail_path <-
+      paste(
+        './r_script/result/differentLearner/',
+        name,
+        '_',
+        temp,
+        '_DelayedCVIdeal_5Fold_FF0.99_detail.csv',
+        sep = ''
+      )
+
+    command <-
+      paste(
+        'java -classpath "classes" moa.DoTask EvaluatePrequentialDelayedCVIdeal -l ',
+        learner ,
+        ' -s "(ArffFileStream" -f "',
+        project ,
+        ')" -e "(FadingFactorClassificationPerformanceEvaluator" -a 0.99 -o -p -r "-f)" -k 99 -f ',
+        f_sampleFrequency ,
+        ' -q ',
+        q_timeFrequency ,
+        ' -d "',
+        dump_path ,
+        '" -o ',
+        '"',
+        detail_path ,
+        '"',
+        ' -a Bootstrap-Validation  -D 0 -w 5 -A 1 -r 2 ',
+        sep = ''
+      )
+    
+    
+    seconds_in_a_day <- 24*60*60
+    P <- seconds_in_a_day * 1
+    N <- seconds_in_a_day * 90
+    command <- combineCommand(learner,seed,f_sampleFrequency,q_timeFrequency,project,P,N)
+
+    system(command, wait = F)
+    
   }
 }
-
-
-
-
